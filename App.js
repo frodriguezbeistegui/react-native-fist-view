3ffe96ae52676ad837eda04426cbea236d140c74
frodriguezbeistegui <frodriguezbeistegui@gmail.com>
2023-11-06 16:18:45 -0300

    final commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
+import { StyleSheet, View, FlatList, Button, Text } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 import { StatusBar } from 'expo-status-bar';
@@ -40,24 +40,29 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
+
       <View style={ styles.appCointainer }>
-        <Button title='Add New Goal' color="#a065ec" onPress={startAddGoalHandler} />
         <GoalInput addGoalHandler={addGoalHandler} showModal={modalIsVisible} onCancel={endAddGoalHander} />
-        <FlatList
-              alwaysBounceVertical={false}
-              style={styles.goalsContainer}
-              data={courseGoals}
-              renderItem={({item})=>{
-                  return(
-                  <GoalItem
-                    text={item.text}
-                    onDeletetItem={deleteGoalHander}
-                    id={item.id}
-                  />
-                  )
-              }}
-              keyExtractor={(item, index) => item.id}
-              />      
+        <View style={styles.titleContainer} >
+          <Text style={styles.titleText} >Course Goals:</Text>
+        </View>
+        <View style={styles.goalsContainer}>
+          <FlatList
+            alwaysBounceVertical={false}
+            data={courseGoals}
+            renderItem={({item})=>{
+                return(
+                <GoalItem
+                  text={item.text}
+                  onDeletetItem={deleteGoalHander}
+                  id={item.id}
+                />
+                )
+            }}
+            keyExtractor={(item) => item.id}
+           />  
+        </View>
+        <Button title='Add New Goal' color="#a065ec" onPress={startAddGoalHandler}  />
       </View>
     </>
   );
@@ -66,11 +71,21 @@ export default function App() {
 const styles = StyleSheet.create({ 
   appCointainer: {
     flex: 1,
-    paddingTop: 50,
-    paddingVertical: 40,
+    paddingTop: 60,
     backgroundColor: '#1e085a',
   },
   goalsContainer: {
     flex: 5,
+    marginVertical: 20
   },
+  titleText: {
+    fontSize: 30,
+    fontWeight: '800',
+    color: '#fafafa'
+  },
+  titleContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+    flex: 1,
+  } 
 });
